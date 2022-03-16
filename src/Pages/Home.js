853fc4c639a303af2ce127544e174c03401173e7
Lalith Reddy <lalithpunepalli@gmail.com>
2022-03-16 22:07:13 +0530

    warnings fixed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,18 +11,9 @@ function Home() {
     return (<div>
         <div id="navbar">
             <label className="page-title">FIND YOUR BANK</label>
-            <a className={favoriteActive ? "active" : ""} onClick={() => { setFavoriteActive(true) }}>Favorites</a>
-            <a className={!favoriteActive ? "active" : ""} onClick={() => setFavoriteActive(false)}>All Banks</a>
+            <button className={favoriteActive ? "active-button" : ""} onClick={() => { setFavoriteActive(true) }}>Favorites</button>
+            <button className={!favoriteActive ? "active-button" : ""} onClick={() => setFavoriteActive(false)}>All Banks</button>
         </div>
-        {/* <div style={{ clear: "both" }}></div>
-        <div className="top-nav-bar">
-            <h2 className="page-title">FIND YOUR BANK</h2>
-            <div className="menu-links">
-                <button className={!favoriteActive ? "btn btn-outline-primary shadow-none active-button" : "btn btn-outline-primary button"} onClick={() => { setFavoriteActive(false) }}>All Banks</button>
-                <button className={favoriteActive ? "btn btn-outline-primary  shadow-none active-button" : "btn btn-outline-primary button"} onClick={() => setFavoriteActive(true)}>Favorites</button>
-            </div>
-        </div>
-        <div style={{ clear: "both" }}></div> */}
         <div style={{ clear: "both" }}></div>
         {!favoriteActive && <AllBanksPage></AllBanksPage>}
         {favoriteActive && <FavoritesPage></FavoritesPage>}
